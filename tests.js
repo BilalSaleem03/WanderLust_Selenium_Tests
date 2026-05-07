4737d3d38f2f2bf20bf8f1e19d080451619c4633
Muhammad Bilal Saleem <155255932+BilalSaleem03@users.noreply.github.com>
2026-05-07 23:45:19 +0500

    add tests

--- tests.js
+++ tests.js
@@ -53,6 +53,48 @@ async function runTests() {
             throw new Error("Test 2 Failed: Error message not displayed.");
         }
 
+
+        console.log("Starting Test 3: Failed Signup...");
+
+        baseUrl = 'http://3.27.160.224:3001/signup'
+        await driver.get(baseUrl);
+
+        await driver.findElement(By.name('username')).sendKeys('demo');
+        await driver.findElement(By.name('email')).sendKeys('demo@gmail.com');
+        await driver.findElement(By.name('password')).sendKeys('demo');
+        await driver.findElement(By.css('.btn-light')).click();
+
+        // Check for error flash message
+        let errorFlash = await driver.wait(until.elementLocated(By.css('.alert-danger')), 5000);
+        let errorText = await errorFlash.getText();
+
+        if (errorText.length > 0) {
+            console.log("✔ Test 3 Passed: Error message displayed...");
+        } else {
+            throw new Error("Test 3 Failed: Error message not displayed.");
+        }
+        
+        
+        console.log("Starting Test 4: Success Signup...");
+
+        baseUrl = 'http://3.27.160.224:3001/signup'
+        await driver.get(baseUrl);
+
+        await driver.findElement(By.name('username')).sendKeys('demo5');
+        await driver.findElement(By.name('email')).sendKeys('demo5@gmail.com');
+        await driver.findElement(By.name('password')).sendKeys('demo5');
+        await driver.findElement(By.css('.btn-light')).click();
+
+        // Check for error flash message
+        let successFlash = await driver.wait(until.elementLocated(By.css('.alert-success')), 5000);
+        let successText = await successFlash.getText();
+
+        if (successText.length > 0) {
+            console.log("✔ Test 3 Passed: Success message displayed...");
+        } else {
+            throw new Error("Test 3 Failed: Success message not displayed.");
+        }
+
     } catch (err) {
         console.error("Pipeline Test Failure:", err.message);
         process.exit(1); // Forces Jenkins to mark the 'Test' stage as failed [cite: 16]
